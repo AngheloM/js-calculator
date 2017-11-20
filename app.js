32e79ba54e0541a8ef7e84c62fc408636a481a66
AngheloM <macha.anghelo@gmail.com>
2017-11-20 11:14:53 -1000

    finished

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 var output=null;
 function calculator (x,y,z){
+  if (x === "+") {
+    output = y + z;
+  } else if (x === "-") {
+    output = y - z;
+  } else if (x === "/") {
+    output = y/z;
+  } else if (x === "*") {
+    output = y*z;
+  }
+} 
 
-}
+calculator("-", 10, 5);
+console.log(output);
